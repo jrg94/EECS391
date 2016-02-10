136e9c2287b92dbbc415715cab01abc0e155b869
jrg94 <jrg170@case.edu>
2016-02-10 03:05:37 -0500

    Making sure that isValidMapLocation is performing correctly

--- src/edu/cwru/sepia/agent/AstarAgentTest.java
+++ src/edu/cwru/sepia/agent/AstarAgentTest.java
@@ -13,8 +13,14 @@ public class AstarAgentTest {
 	@Test
 	public void testIsValidMapLocation() {
 		AstarAgent a = new AstarAgent(1);
-		boolean test = a.isValidMapLocation(new AstarAgent.MapLocation(1,1, null, 0), new AstarAgent.MapLocation(1,2, null, 0), 10, 10, new HashSet<MapLocation>());
-		assertTrue(test);
+		
+		boolean testInsideGridNotBlocked = a.isValidMapLocation(new AstarAgent.MapLocation(1,1, null, 0), new AstarAgent.MapLocation(1,2, null, 0), 10, 10, new HashSet<MapLocation>());
+		assertTrue(testInsideGridNotBlocked);
+		
+		Set<MapLocation> resources = new HashSet<MapLocation>();
+		resources.add(new AstarAgent.MapLocation(1,1,null,0));
+		boolean testOutsideGrid = a.isValidMapLocation(new AstarAgent.MapLocation(0,0, null, 0), new AstarAgent.MapLocation(-1,0, null, 0), 10, 10, new HashSet<MapLocation>());
+		assertFalse(testOutsideGrid);
 	}
 
 }
