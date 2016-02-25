35d8481dd7ca9f6fe6fc58fca4b2420581f22bb9
Ted Kim <tkk21@case.edu>
2016-02-25 13:58:06 -0500

    init

---
+++ src/edu/cwru/sepia/agent/minimax/GameState.java
@@ -0,0 +1,88 @@
+package edu.cwru.sepia.agent.minimax;
+
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.action.ActionType;
+import edu.cwru.sepia.action.DirectedAction;
+import edu.cwru.sepia.action.TargetedAction;
+import edu.cwru.sepia.environment.model.state.State;
+import edu.cwru.sepia.environment.model.state.Unit;
+import edu.cwru.sepia.util.Direction;
+
+import java.util.*;
+
+/**
+ * This class stores all of the information the agent
+ * needs to know about the state of the game. For example this
+ * might include things like footmen HP and positions.
+ *
+ * Add any information or methods you would like to this class,
+ * but do not delete or change the signatures of the provided methods.
+ */
+public class GameState {
+
+    /**
+     * You will implement this constructor. It will
+     * extract all of the needed state information from the built in
+     * SEPIA state view.
+     *
+     * You may find the following state methods useful:
+     *
+     * state.getXExtent() and state.getYExtent(): get the map dimensions
+     * state.getAllResourceIDs(): returns all of the obstacles in the map
+     * state.getResourceNode(Integer resourceID): Return a ResourceView for the given ID
+     *
+     * For a given ResourceView you can query the position using
+     * resource.getXPosition() and resource.getYPosition()
+     *
+     * For a given unit you will need to find the attack damage, range and max HP
+     * unitView.getTemplateView().getRange(): This gives you the attack range
+     * unitView.getTemplateView().getBasicAttack(): The amount of damage this unit deals
+     * unitView.getTemplateView().getBaseHealth(): The maximum amount of health of this unit
+     *
+     * @param state Current state of the episode
+     */
+    public GameState(State.StateView state) {
+    }
+
+    /**
+     * You will implement this function.
+     *
+     * You should use weighted linear combination of features.
+     * The features may be primitives from the state (such as hp of a unit)
+     * or they may be higher level summaries of information from the state such
+     * as distance to a specific location. Come up with whatever features you think
+     * are useful and weight them appropriately.
+     *
+     * It is recommended that you start simple until you have your algorithm working. Then watch
+     * your agent play and try to add features that correct mistakes it makes. However, remember that
+     * your features should be as fast as possible to compute. If the features are slow then you will be
+     * able to do less plys in a turn.
+     *
+     * Add a good comment about what is in your utility and why you chose those features.
+     *
+     * @return The weighted linear combination of the features
+     */
+    public double getUtility() {
+        return 0.0;
+    }
+
+    /**
+     * You will implement this function.
+     *
+     * This will return a list of GameStateChild objects. You will generate all of the possible
+     * actions in a step and then determine the resulting game state from that action. These are your GameStateChildren.
+     *
+     * You may find it useful to iterate over all the different directions in SEPIA.
+     *
+     * for(Direction direction : Directions.values())
+     *
+     * To get the resulting position from a move in that direction you can do the following
+     * x += direction.xComponent()
+     * y += direction.yComponent()
+     *
+     * @return All possible actions and their associated resulting game state
+     */
+    public List<GameStateChild> getChildren() {
+        return null;
+    }
+}
---
+++ src/edu/cwru/sepia/agent/minimax/MinimaxAlphaBeta.java
@@ -0,0 +1,97 @@
+package edu.cwru.sepia.agent.minimax;
+
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.agent.Agent;
+import edu.cwru.sepia.environment.model.history.History;
+import edu.cwru.sepia.environment.model.state.State;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+
+public class MinimaxAlphaBeta extends Agent {
+
+    private final int numPlys;
+
+    public MinimaxAlphaBeta(int playernum, String[] args)
+    {
+        super(playernum);
+
+        if(args.length < 1)
+        {
+            System.err.println("You must specify the number of plys");
+            System.exit(1);
+        }
+
+        numPlys = Integer.parseInt(args[0]);
+    }
+
+    @Override
+    public Map<Integer, Action> initialStep(State.StateView newstate, History.HistoryView statehistory) {
+        return middleStep(newstate, statehistory);
+    }
+
+    @Override
+    public Map<Integer, Action> middleStep(State.StateView newstate, History.HistoryView statehistory) {
+        GameStateChild bestChild = alphaBetaSearch(new GameStateChild(newstate),
+                numPlys,
+                Double.NEGATIVE_INFINITY,
+                Double.POSITIVE_INFINITY);
+
+        return bestChild.action;
+    }
+
+    @Override
+    public void terminalStep(State.StateView newstate, History.HistoryView statehistory) {
+
+    }
+
+    @Override
+    public void savePlayerData(OutputStream os) {
+
+    }
+
+    @Override
+    public void loadPlayerData(InputStream is) {
+
+    }
+
+    /**
+     * You will implement this.
+     *
+     * This is the main entry point to the alpha beta search. Refer to the slides, assignment description
+     * and book for more information.
+     *
+     * Try to keep the logic in this function as abstract as possible (i.e. move as much SEPIA specific
+     * code into other functions and methods)
+     *
+     * @param node The action and state to search from
+     * @param depth The remaining number of plys under this node
+     * @param alpha The current best value for the maximizing node from this node to the root
+     * @param beta The current best value for the minimizing node from this node to the root
+     * @return The best child of this node with updated values
+     */
+    public GameStateChild alphaBetaSearch(GameStateChild node, int depth, double alpha, double beta)
+    {
+        return node;
+    }
+
+    /**
+     * You will implement this.
+     *
+     * Given a list of children you will order them according to heuristics you make up.
+     * See the assignment description for suggestions on heuristics to use when sorting.
+     *
+     * Use this function inside of your alphaBetaSearch method.
+     *
+     * Include a good comment about what your heuristics are and why you chose them.
+     *
+     * @param children
+     * @return The list of children sorted by your heuristic.
+     */
+    public List<GameStateChild> orderChildrenWithHeuristics(List<GameStateChild> children)
+    {
+        return children;
+    }
+}
