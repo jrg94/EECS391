5ab4d32607ce8ae54508cb7dadb56072daa7cda1
Ted <tkk21@case.edu>
2016-03-28 18:15:37 -0400

    move action implemented

--- src/edu/cwru/sepia/agent/planner/actions/MoveAction.java
+++ src/edu/cwru/sepia/agent/planner/actions/MoveAction.java
@@ -1,19 +1,32 @@
 package edu.cwru.sepia.agent.planner.actions;
 
 import edu.cwru.sepia.agent.planner.GameState;
+import edu.cwru.sepia.agent.planner.PeasantSimulation;
+import edu.cwru.sepia.agent.planner.Position;
 
 public class MoveAction implements StripsAction{
 
+	private PeasantSimulation peasant;
+	private Position destinationPosition;
+	
+	public MoveAction(PeasantSimulation peasant, Position destinationPosition){
+		this.peasant = peasant;
+		this.destinationPosition = destinationPosition;
+	}
+	
 	@Override
 	public boolean preconditionsMet(GameState state) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return !peasant.getPosition().equals(destinationPosition);
+		//TODO add another condition that checks if there's already a peasant in the way
 	}
 
 	@Override
 	public GameState apply(GameState state) {
-		// TODO Auto-generated method stub
-		return null;
+		PeasantSimulation peasantClone = new PeasantSimulation(destinationPosition, peasant.getCargo(), peasant.getCargoType(), peasant.getUnitId());
+		GameState nextGameState = new GameState(state, this);
+		nextGameState.getPeasantMap().put(peasantClone.getUnitId(), peasantClone);
+		return nextGameState;
 	}
 
 }
