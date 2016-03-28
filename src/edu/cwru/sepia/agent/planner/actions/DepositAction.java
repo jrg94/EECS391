ef1d3d4133d650c6f40ac3b3830878414b4dc248
Ted <tkk21@case.edu>
2016-03-28 19:09:56 -0400

    implemented deposit action

--- src/edu/cwru/sepia/agent/planner/actions/DepositAction.java
+++ src/edu/cwru/sepia/agent/planner/actions/DepositAction.java
@@ -1,19 +1,43 @@
 package edu.cwru.sepia.agent.planner.actions;
 
 import edu.cwru.sepia.agent.planner.GameState;
+import edu.cwru.sepia.agent.planner.PeasantSimulation;
+import edu.cwru.sepia.environment.model.state.ResourceType;
 
 public class DepositAction implements StripsAction {
 
+	private PeasantSimulation peasant;
+	
+	public DepositAction(PeasantSimulation peasant) {
+		this.peasant = peasant;
+	}
+	
 	@Override
 	public boolean preconditionsMet(GameState state) {
-		// TODO Auto-generated method stub
-		return false;
+		return peasant.getPosition().isAdjacent(state.getTownHall().getPosition())
+				&& peasant.isCarrying();
 	}
 
 	@Override
 	public GameState apply(GameState state) {
-		// TODO Auto-generated method stub
-		return null;
+		GameState nextGameState = new GameState(state, this);
+		
+		ResourceType holdingType = peasant.getCargoType();
+		int holdingAmount = peasant.getCargo();
+		
+		PeasantSimulation peasantClone = new PeasantSimulation(peasant.getPosition(), 0, null, peasant.getUnitId());
+		nextGameState.getPeasantMap().put(peasantClone.getUnitId(), peasantClone);
+		
+		// if i get null pointer error here, it's cuz isCarrying doesn't work
+		switch(holdingType){
+		case GOLD:
+			nextGameState.setRequiredGold(nextGameState.getRequiredGold() - holdingAmount);
+			break;
+		case WOOD:
+			nextGameState.setRequiredWood(nextGameState.getRequiredWood() - holdingAmount);
+			break;
+		}
+		return nextGameState;
 	}
 
 }
