5ab292734b5a52e351789d93bb64b13efee83334
Ted <tkk21@case.edu>
2016-03-28 17:36:12 -0400

    equals, hashcode, tostring for resource sim

--- src/edu/cwru/sepia/agent/planner/ResourceSimulation.java
+++ src/edu/cwru/sepia/agent/planner/ResourceSimulation.java
@@ -40,5 +40,52 @@ public class ResourceSimulation {
 	 */
 	public ResourceNode.Type getResourceType() {
 		return resourceType;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((position == null) ? 0 : position.hashCode());
+		result = prime * result + resourceRemaining;
+		result = prime * result + ((resourceType == null) ? 0 : resourceType.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ResourceSimulation other = (ResourceSimulation) obj;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		if (resourceRemaining != other.resourceRemaining)
+			return false;
+		if (resourceType != other.resourceType)
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "ResourceSimulation [position=" + position + ", resourceRemaining=" + resourceRemaining
+				+ ", resourceType=" + resourceType + "]";
 	}	
+	
 }
