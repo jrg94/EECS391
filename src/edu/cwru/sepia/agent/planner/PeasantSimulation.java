91be4726486e718aa543f4d02736629af05b794b
Ted <tkk21@case.edu>
2016-03-28 17:23:23 -0400

    peasant now record unit id

--- src/edu/cwru/sepia/agent/planner/PeasantSimulation.java
+++ src/edu/cwru/sepia/agent/planner/PeasantSimulation.java
@@ -5,22 +5,32 @@ import edu.cwru.sepia.environment.model.state.Unit.UnitView;
 
 public class PeasantSimulation {
 
+	private int unitId;
 	private Position position;
 	private int cargo;
 	private ResourceType cargoType;
 	
 	public PeasantSimulation(UnitView unit){
+		unitId = unit.getID();
 		position = new Position(unit.getXPosition(), unit.getYPosition());
 		cargo = unit.getCargoAmount();
 		cargoType = unit.getCargoType();
 	}
 	
-	public PeasantSimulation(Position pos, int cargo, ResourceType cargoType){
+	public PeasantSimulation(Position pos, int cargo, ResourceType cargoType, int unitId){
 		position = new Position(pos);
 		this.cargo = cargo;
 		this.cargoType = cargoType;
+		this.unitId = unitId;
 	}
 	
+	/**
+	 * @return the unitId
+	 */
+	public int getUnitId() {
+		return unitId;
+	}
+
 	/**
 	 * @return the position
 	 */
