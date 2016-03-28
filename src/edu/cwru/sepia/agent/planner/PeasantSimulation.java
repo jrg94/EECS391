28cb3137980ea338f25346c434931b85632fcdba
Ted <tkk21@case.edu>
2016-03-28 17:35:19 -0400

    equals, hashcode, tostring for peasant sim

--- src/edu/cwru/sepia/agent/planner/PeasantSimulation.java
+++ src/edu/cwru/sepia/agent/planner/PeasantSimulation.java
@@ -52,4 +52,48 @@ public class PeasantSimulation {
 		return cargoType;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PeasantSimulation other = (PeasantSimulation) obj;
+		if (cargo != other.cargo)
+			return false;
+		if (cargoType != other.cargoType)
+			return false;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		if (unitId != other.unitId)
+			return false;
+		return true;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + cargo;
+		result = prime * result + ((cargoType == null) ? 0 : cargoType.hashCode());
+		result = prime * result + ((position == null) ? 0 : position.hashCode());
+		result = prime * result + unitId;
+		return result;
+	}
+	
+	@Override
+	public String toString(){
+		return String.format("PeasantSimulation:{id=%d, position=%s, cargoType=%s, cargo=%d}", unitId, position.toString(), cargoType.toString(), cargo);
+	}
 }
