b145bdb447b5df655fcd1c146c542991111de189
Ted <tkk21@case.edu>
2016-03-28 17:04:12 -0400

    add in all the stirps actions

---
+++ src/edu/cwru/sepia/agent/planner/actions/HarvestAction.java
@@ -0,0 +1,19 @@
+package edu.cwru.sepia.agent.planner.actions;
+
+import edu.cwru.sepia.agent.planner.GameState;
+
+public class HarvestAction implements StripsAction{
+
+	@Override
+	public boolean preconditionsMet(GameState state) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public GameState apply(GameState state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
