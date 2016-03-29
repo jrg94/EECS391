a1beb720aa1e825d0aaed2cdd95f31576631f6dd
Ted <tkk21@case.edu>
2016-03-29 01:26:12 -0400

    added build peasant action stub

---
+++ src/edu/cwru/sepia/agent/planner/actions/BuildPeasantAction.java
@@ -0,0 +1,25 @@
+package edu.cwru.sepia.agent.planner.actions;
+
+import edu.cwru.sepia.agent.planner.GameState;
+
+public class BuildPeasantAction implements StripsAction{
+
+	@Override
+	public boolean preconditionsMet(GameState state) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public GameState apply(GameState state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int cost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
