3bad919d03c3cad74a8398def8797d06994533ad
Ted Kim <tkk21@case.edu>
2016-03-29 13:00:50 -0400

    idk tabs are really scary

--- src/edu/cwru/sepia/agent/planner/PEAgent.java
+++ src/edu/cwru/sepia/agent/planner/PEAgent.java
@@ -22,149 +22,147 @@ import java.util.Stack;
  */
 public class PEAgent extends Agent {
 
-    // The plan being executed
-    private Stack<StripsAction> plan = null;
+	// The plan being executed
+	private Stack<StripsAction> plan = null;
 
-    // maps the real unit Ids to the plan's unit ids
-    // when you're planning you won't know the true unit IDs that sepia assigns. So you'll use placeholders (1, 2, 3).
-    // this maps those placeholders to the actual unit IDs.
-    private Map<Integer, Integer> peasantIdMap;
-    private int townhallId;
-    private Position townhallPosition;
-    private int peasantTemplateId;
+	// maps the real unit Ids to the plan's unit ids
+	// when you're planning you won't know the true unit IDs that sepia assigns. So you'll use placeholders (1, 2, 3).
+	// this maps those placeholders to the actual unit IDs.
+	private Map<Integer, Integer> peasantIdMap;
+	private int townhallId;
+	private Position townhallPosition;
+	private int peasantTemplateId;
 
-    public PEAgent(int playernum, Stack<StripsAction> plan) {
-        super(playernum);
-        peasantIdMap = new HashMap<Integer, Integer>();
-        this.plan = plan;
+	public PEAgent(int playernum, Stack<StripsAction> plan) {
+		super(playernum);
+		peasantIdMap = new HashMap<Integer, Integer>();
+		this.plan = plan;
 
-    }
+	}
 
-    @Override
-    public Map<Integer, Action> initialStep(State.StateView stateView, History.HistoryView historyView) {
-        // gets the townhall ID and the peasant ID
-        for(int unitId : stateView.getUnitIds(playernum)) {
-            Unit.UnitView unit = stateView.getUnit(unitId);
-            String unitType = unit.getTemplateView().getName().toLowerCase();
-            if(unitType.equals("townhall")) {
-                townhallId = unitId;
-                townhallPosition = new Position(unit.getXPosition(), unit.getYPosition());
-            } else if(unitType.equals("peasant")) {
-                peasantIdMap.put(unitId, unitId);
-            }
-        }
+	@Override
+	public Map<Integer, Action> initialStep(State.StateView stateView, History.HistoryView historyView) {
+		// gets the townhall ID and the peasant ID
+		for(int unitId : stateView.getUnitIds(playernum)) {
+			Unit.UnitView unit = stateView.getUnit(unitId);
+			String unitType = unit.getTemplateView().getName().toLowerCase();
+			if(unitType.equals("townhall")) {
+				townhallId = unitId;
+				townhallPosition = new Position(unit.getXPosition(), unit.getYPosition());
+			} else if(unitType.equals("peasant")) {
+				peasantIdMap.put(unitId, unitId);
+			}
+		}
 
-        // Gets the peasant template ID. This is used when building a new peasant with the townhall
-        for(Template.TemplateView templateView : stateView.getTemplates(playernum)) {
-            if(templateView.getName().toLowerCase().equals("peasant")) {
-                peasantTemplateId = templateView.getID();
-                break;
-            }
-        }
+		// Gets the peasant template ID. This is used when building a new peasant with the townhall
+		for(Template.TemplateView templateView : stateView.getTemplates(playernum)) {
+			if(templateView.getName().toLowerCase().equals("peasant")) {
+				peasantTemplateId = templateView.getID();
+				break;
+			}
+		}
 
-        return middleStep(stateView, historyView);
-    }
+		return middleStep(stateView, historyView);
+	}
 
-    /**
-     * This is where you will read the provided plan and execute it. If your plan is correct then when the plan is empty
-     * the scenario should end with a victory. If the scenario keeps running after you run out of actions to execute
-     * then either your plan is incorrect or your execution of the plan has a bug.
-     *
-     * You can create a SEPIA deposit action with the following method
-     * Action.createPrimitiveDeposit(int peasantId, Direction townhallDirection)
-     *
-     * You can create a SEPIA harvest action with the following method
-     * Action.createPrimitiveGather(int peasantId, Direction resourceDirection)
-     *
-     * You can create a SEPIA build action with the following method
-     * Action.createPrimitiveProduction(int townhallId, int peasantTemplateId)
-     *
-     * You can create a SEPIA move action with the following method
-     * Action.createCompoundMove(int peasantId, int x, int y)
-     *
-     * these actions are stored in a mapping between the peasant unit ID executing the action and the action you created.
-     *
-     * For the compound actions you will need to check their progress and wait until they are complete before issuing
-     * another action for that unit. If you issue an action before the compound action is complete then the peasant
-     * will stop what it was doing and begin executing the new action.
-     *
-     * To check an action's progress you can call getCurrentDurativeAction on each UnitView. If the Action is null nothing
-     * is being executed. If the action is not null then you should also call getCurrentDurativeProgress. If the value is less than
-     * 1 then the action is still in progress.
-     *
-     * Also remember to check your plan's preconditions before executing!
-     */
-    @Override
-    public Map<Integer, Action> middleStep(State.StateView stateView, History.HistoryView historyView) {
-    	Map<Integer, Action> sepiaActions = new HashMap<Integer, Action>();
-    	boolean doneMoving = true;
-    	Action action = null;
-    	if (stateView.getTurnNumber()!=0){
-    		Map<Integer, ActionResult> actionResults = historyView.getCommandFeedback(playernum, stateView.getTurnNumber()-1);
-    		  for (ActionResult result : actionResults.values()) {
-    			  switch(result.getFeedback()){
-    			  case INCOMPLETE:
-    				  action = result.getAction();
-    				  break;
-    			  case INCOMPLETEMAYBESTUCK:
-    				  System.out.println("[PEAgent] may be stuck, unitID= "+action.getUnitId());
-    			  case COMPLETED:
-    				  break;
-    			  case FAILED:
-    				  System.out.println("[PEAgent] failed an action");
-    				  break;
-    			  }
-    			  	
-    			    System.out.println(result.toString());
+	/**
+	 * This is where you will read the provided plan and execute it. If your plan is correct then when the plan is empty
+	 * the scenario should end with a victory. If the scenario keeps running after you run out of actions to execute
+	 * then either your plan is incorrect or your execution of the plan has a bug.
+	 *
+	 * You can create a SEPIA deposit action with the following method
+	 * Action.createPrimitiveDeposit(int peasantId, Direction townhallDirection)
+	 *
+	 * You can create a SEPIA harvest action with the following method
+	 * Action.createPrimitiveGather(int peasantId, Direction resourceDirection)
+	 *
+	 * You can create a SEPIA build action with the following method
+	 * Action.createPrimitiveProduction(int townhallId, int peasantTemplateId)
+	 *
+	 * You can create a SEPIA move action with the following method
+	 * Action.createCompoundMove(int peasantId, int x, int y)
+	 *
+	 * these actions are stored in a mapping between the peasant unit ID executing the action and the action you created.
+	 *
+	 * For the compound actions you will need to check their progress and wait until they are complete before issuing
+	 * another action for that unit. If you issue an action before the compound action is complete then the peasant
+	 * will stop what it was doing and begin executing the new action.
+	 *
+	 * To check an action's progress you can call getCurrentDurativeAction on each UnitView. If the Action is null nothing
+	 * is being executed. If the action is not null then you should also call getCurrentDurativeProgress. If the value is less than
+	 * 1 then the action is still in progress.
+	 *
+	 * Also remember to check your plan's preconditions before executing!
+	 */
+	@Override
+	public Map<Integer, Action> middleStep(State.StateView stateView, History.HistoryView historyView) {
+		Map<Integer, Action> sepiaActions = new HashMap<Integer, Action>();
+		Action action = null;
+		if (stateView.getTurnNumber()!=0){
+			Map<Integer, ActionResult> actionResults = historyView.getCommandFeedback(playernum, stateView.getTurnNumber()-1);
+			for (ActionResult result : actionResults.values()) {
+				switch(result.getFeedback()){
+				case INCOMPLETE:
+					action = result.getAction();
+					break;
+				case INCOMPLETEMAYBESTUCK:
+					System.out.println("[PEAgent] may be stuck, unitID= "+action.getUnitId());
+				case COMPLETED:
+					break;
+				case FAILED:
+					System.out.println("[PEAgent] failed an action");
+					break;
+				default:
+					System.out.println("[PEAgent] default case with the feedback: " + result.getFeedback().toString());
+					break;
+				}
+			}
+		}
+		if (action == null){
+			action = createSepiaAction(plan.pop());
+		}
+		sepiaActions.put(action.getUnitId(), action);
+		return sepiaActions;
+	}
 
-    			  }
-    		
-    	}
-    	if (action == null){
-    		action = createSepiaAction(plan.pop());
-    	}
-    	sepiaActions.put(action.getUnitId(), action);
-        return sepiaActions;
-    }
+	/**
+	 * Returns a SEPIA version of the specified Strips Action.
+	 * @param action StripsAction
+	 * @return SEPIA representation of same action
+	 */
+	private Action createSepiaAction(StripsAction action) {
 
-    /**
-     * Returns a SEPIA version of the specified Strips Action.
-     * @param action StripsAction
-     * @return SEPIA representation of same action
-     */
-    private Action createSepiaAction(StripsAction action) {
-    	
-    	if (action instanceof DepositAction){
-    		DepositAction depositAction = ((DepositAction) action);
-    		PeasantSimulation peasant = depositAction.getPeasant();
-    		return Action.createPrimitiveDeposit(peasant.getUnitId(), peasant.getPosition().getDirection(townhallPosition));
-    	}
-    	else if (action instanceof HarvestAction){
-    		HarvestAction harvestAction = ((HarvestAction)action);
-    		PeasantSimulation peasant = harvestAction.getPeasant();
-    		return Action.createPrimitiveGather(peasant.getUnitId(), peasant.getPosition().getDirection(harvestAction.getResource().getPosition()));
-    	}
-    	else if (action instanceof MoveAction){
-    		MoveAction moveAction = ((MoveAction)action);
-    		PeasantSimulation peasant = moveAction.getPeasant();
-    		return Action.createCompoundMove(peasant.getUnitId(), moveAction.getDestinationPosition().x, moveAction.getDestinationPosition().y);
-    	}
-    	System.out.println("[PEAgent] Invalid StripsAction was entered in createSepiaAction");
-        return null;
-    }
+		if (action instanceof DepositAction){
+			DepositAction depositAction = ((DepositAction) action);
+			PeasantSimulation peasant = depositAction.getPeasant();
+			return Action.createPrimitiveDeposit(peasant.getUnitId(), peasant.getPosition().getDirection(townhallPosition));
+		}
+		else if (action instanceof HarvestAction){
+			HarvestAction harvestAction = ((HarvestAction)action);
+			PeasantSimulation peasant = harvestAction.getPeasant();
+			return Action.createPrimitiveGather(peasant.getUnitId(), peasant.getPosition().getDirection(harvestAction.getResource().getPosition()));
+		}
+		else if (action instanceof MoveAction){
+			MoveAction moveAction = ((MoveAction)action);
+			PeasantSimulation peasant = moveAction.getPeasant();
+			return Action.createCompoundMove(peasant.getUnitId(), moveAction.getDestinationPosition().x, moveAction.getDestinationPosition().y);
+		}
+		System.out.println("[PEAgent] Invalid StripsAction was entered in createSepiaAction");
+		return null;
+	}
 
-    @Override
-    public void terminalStep(State.StateView stateView, History.HistoryView historyView) {
+	@Override
+	public void terminalStep(State.StateView stateView, History.HistoryView historyView) {
 
-    }
+	}
 
-    @Override
-    public void savePlayerData(OutputStream outputStream) {
+	@Override
+	public void savePlayerData(OutputStream outputStream) {
 
-    }
+	}
 
-    @Override
-    public void loadPlayerData(InputStream inputStream) {
+	@Override
+	public void loadPlayerData(InputStream inputStream) {
 
-    }
+	}
 }
