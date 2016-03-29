2db4904423bcbc9c71a91ee4a8345bb80081f692
Ted <tkk21@case.edu>
2016-03-28 21:03:05 -0400

    equals and hashcode implemented

--- src/edu/cwru/sepia/agent/planner/GameState.java
+++ src/edu/cwru/sepia/agent/planner/GameState.java
@@ -262,28 +262,71 @@ public class GameState implements Comparable<GameState> {
         return 0;
     }
 
-    /**
-     * This will be necessary to use the GameState as a key in a Set or Map.
-     *
-     * @param o The game state to compare
-     * @return True if this state equals the other state, false otherwise.
-     */
-    @Override
-    public boolean equals(Object o) {
-        // TODO: Implement me!
-        return false;
-    }
+    /* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameState other = (GameState) obj;
+		if (action == null) {
+			if (other.action != null)
+				return false;
+		} else if (!action.equals(other.action))
+			return false;
+		if (buildPeasants != other.buildPeasants)
+			return false;
+		if (parent == null) {
+			if (other.parent != null)
+				return false;
+		} else if (!parent.equals(other.parent))
+			return false;
+		if (peasantMap == null) {
+			if (other.peasantMap != null)
+				return false;
+		} else if (!peasantMap.equals(other.peasantMap))
+			return false;
+		if (playerNum != other.playerNum)
+			return false;
+		if (requiredGold != other.requiredGold)
+			return false;
+		if (requiredWood != other.requiredWood)
+			return false;
+		if (resourceMap == null) {
+			if (other.resourceMap != null)
+				return false;
+		} else if (!resourceMap.equals(other.resourceMap))
+			return false;
+		if (townHall == null) {
+			if (other.townHall != null)
+				return false;
+		} else if (!townHall.equals(other.townHall))
+			return false;
+		return true;
+	}
 
-    /**
-     * This is necessary to use the GameState as a key in a HashSet or HashMap. Remember that if two objects are
-     * equal they should hash to the same value.
-     *
-     * @return An integer hashcode that is equal for equal states.
-     */
-    @Override
-    public int hashCode() {
-        // TODO: Implement me!
-        return 0;
-    }
+    /* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((action == null) ? 0 : action.hashCode());
+		result = prime * result + (buildPeasants ? 1231 : 1237);
+		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+		result = prime * result + ((peasantMap == null) ? 0 : peasantMap.hashCode());
+		result = prime * result + playerNum;
+		result = prime * result + requiredGold;
+		result = prime * result + requiredWood;
+		result = prime * result + ((resourceMap == null) ? 0 : resourceMap.hashCode());
+		result = prime * result + ((townHall == null) ? 0 : townHall.hashCode());
+		return result;
+	}
 
 }
