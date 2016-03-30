14a4dbc6377d732514c3de35dcaf1901f2173528
Ted <tkk21@case.edu>
2016-03-30 00:49:40 -0400

    ghetto implementation of build peasant action

--- src/edu/cwru/sepia/agent/planner/actions/BuildPeasantAction.java
+++ src/edu/cwru/sepia/agent/planner/actions/BuildPeasantAction.java
@@ -1,6 +1,8 @@
 package edu.cwru.sepia.agent.planner.actions;
 
 import edu.cwru.sepia.agent.planner.GameState;
+import edu.cwru.sepia.agent.planner.PeasantSimulation;
+import edu.cwru.sepia.agent.planner.Position;
 
 public class BuildPeasantAction implements StripsAction{
 
@@ -19,19 +21,28 @@ public class BuildPeasantAction implements StripsAction{
 	
 	@Override
 	public boolean preconditionsMet(GameState state) {
-		return currentGold>=PEASANT_GOLD_COST && supplyCap-currentFood >= PEASANT_FOOD_CONSUMPTION;
+		return currentGold>=PEASANT_GOLD_COST && currentFood + PEASANT_FOOD_CONSUMPTION <= supplyCap;
 	}
 
 	@Override
 	public GameState apply(GameState state) {
-		// TODO Auto-generated method stub
-		return null;
+		GameState nextGameState = new GameState(state, this);
+		//TODO actually get townhall's spawn point
+		Position peasantSpawnPoint = new Position(nextGameState.getTownHall().getPosition().getAdjacentPositions().get((int)(8*Math.random())));
+		PeasantSimulation babyPeasant = new PeasantSimulation(peasantSpawnPoint, 0, null, currentFood);//TODO no clue about the peasant id either lol
+		
+		nextGameState.getPeasantMap().put(babyPeasant.getUnitId(), babyPeasant);
+		nextGameState.setRequiredGold(nextGameState.getRequiredGold() + PEASANT_GOLD_COST);
+		nextGameState.setCurrentGold(nextGameState.getCurrentGold() - PEASANT_GOLD_COST);
+		nextGameState.setCurrentFood(nextGameState.getCurrentFood() + PEASANT_FOOD_CONSUMPTION);
+
+		return nextGameState;
 	}
 
 	@Override
 	public int cost() {
 		// TODO Auto-generated method stub
-		return 0;
+		return 1;
 	}
 
 }
